9f79d5429591e1201d12998b6c33ed9473ec5cf5
banstolabrothers <banstolabrothers@gmail.com>
2026-04-05 11:39:54 +0545

    Sanity Updates

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,12 +1,11 @@
 import { createClient } from "@sanity/client";
 import { createImageUrlBuilder } from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "wsx6a0ze",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
   apiVersion: "2021-10-21",
-  useCdn: true,
+  useCdn: false,
 });
 
 const builder = createImageUrlBuilder(client);
